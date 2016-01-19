87f23fc3f4c32baf82b6b929be7f5ca3695ed72b
Wen <Wen@Lins-MacBook-Pro.local>
2016-01-19 12:19:07 +0000

    Update dynamic drawing

--- javascript/src/bongiovi/Mesh.js
+++ javascript/src/bongiovi/Mesh.js
@@ -53,12 +53,14 @@ p.bufferVertex = function(aArrayVertices, isDynamic) {
 		}
 	}
 
+	this.vertexSize = aArrayVertices.length;
 	this.gl.bufferData(this.gl.ARRAY_BUFFER, this._floatArrayVertex, drawType);
 	this.vBufferPos.itemSize = 3;
 };
 
-p.bufferTexCoords = function(aArrayTexCoords) {
+p.bufferTexCoords = function(aArrayTexCoords, isDynamic) {
 	var coords = [];
+	var drawType = isDynamic ? this.gl.DYNAMIC_DRAW : this.gl.STATIC_DRAW;
 
 	for(var i=0; i<aArrayTexCoords.length; i++) {
 		for(var j=0; j<aArrayTexCoords[i].length; j++) {
@@ -68,8 +70,49 @@ p.bufferTexCoords = function(aArrayTexCoords) {
 
 	this.vBufferUV = this.gl.createBuffer();
 	this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vBufferUV);
-	this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(coords), this.gl.STATIC_DRAW);
+	this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(coords), drawType);
 	this.vBufferUV.itemSize = 2;
+};
+
+p.bufferIndices = function(aArrayIndices, isDynamic) {
+	this.indexSize = aArrayIndices.length;
+	var drawType = isDynamic ? this.gl.DYNAMIC_DRAW : this.gl.STATIC_DRAW;
+	this._indices = aArrayIndices;
+	this.iBuffer = this.gl.createBuffer();
+	this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.iBuffer);
+	this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(aArrayIndices), drawType);
+	this.iBuffer.itemSize = 1;
+	this.iBuffer.numItems = aArrayIndices.length;
+};
+
+p.computeNormals = function() {
+	if(this.drawType !== this.gl.TRIANGLES) {return;}
+
+	if(this._faces === undefined) {	this._generateFaces();	}
+	console.log("Start computing");
+
+	var time = new Date().getTime();
+	var j=0;
+
+	this._normals = [];
+	for(var i=0; i<this._vertices.length; i++) {
+		var normal = glm.vec3.create();
+		var faceCount = 0;
+		for(j=0; j<this._faces.length; j++) {
+			if(this._faces[j].contains(this._vertices[i])) {
+				glm.vec3.add(normal, normal, this._faces[j].faceNormal);
+				faceCount ++;
+			}
+		}
+
+		glm.vec3.normalize(normal, normal);
+		this._normals.push(normal);
+	}
+
+	this.bufferData(this._normals, "aNormal", 3);
+
+	var totalTime = new Date().getTime() - time;
+	console.log("Total Time : ", totalTime);
 };
 
 p.bufferData = function(aData, aName, aItemSize, isDynamic) {
@@ -110,46 +153,6 @@ p.bufferData = function(aData, aName, aItemSize, isDynamic) {
 		this.gl.bufferData(this.gl.ARRAY_BUFFER, floatArray, drawType);
 	}
 
-};
-
-p.bufferIndices = function(aArrayIndices) {
-	this._indices = aArrayIndices;
-	this.iBuffer = this.gl.createBuffer();
-	this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.iBuffer);
-	this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(aArrayIndices), this.gl.STATIC_DRAW);
-	this.iBuffer.itemSize = 1;
-	this.iBuffer.numItems = aArrayIndices.length;
-};
-
-
-p.computeNormals = function() {
-	if(this.drawType !== this.gl.TRIANGLES) {return;}
-
-	if(this._faces === undefined) {	this._generateFaces();	}
-	console.log("Start computing");
-
-	var time = new Date().getTime();
-	var j=0;
-
-	this._normals = [];
-	for(var i=0; i<this._vertices.length; i++) {
-		var normal = glm.vec3.create();
-		var faceCount = 0;
-		for(j=0; j<this._faces.length; j++) {
-			if(this._faces[j].contains(this._vertices[i])) {
-				glm.vec3.add(normal, normal, this._faces[j].faceNormal);
-				faceCount ++;
-			}
-		}
-
-		glm.vec3.normalize(normal, normal);
-		this._normals.push(normal);
-	}
-
-	this.bufferData(this._normals, "aNormal", 3);
-
-	var totalTime = new Date().getTime() - time;
-	console.log("Total Time : ", totalTime);
 };
 
 
